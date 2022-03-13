1a16d4f388560fc2d073569f643e9e54caa86d89
NavnathGunjal07 <53387548+NavnathGunjal07@users.noreply.github.com>
2022-03-13 18:21:13 +0530

    Completed cart app

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,74 +1,46 @@
 import React from 'react';
 
-class CartItem extends React.Component {
-  constructor () {
-    super();
-    this.state = {
-      price: 999,
-      title: 'Mobile Phone',
-      qty: 1,
-      img: ''
-    }
-    // this.increaseQuantity = this.increaseQuantity.bind(this);
-  }
-  increaseQuantity = () => {
-    //if previous state is not required use this
-    // this.setState({
-    //   qty:this.state.qty+1
-    // });
-
-    this.setState((prevState)=>{
-      return {
-        qty:prevState.qty+1
-      }
-    });
-
-  }
-  decreaseQuantity = () => {
-    this.setState((prevState)=>{
-      if(prevState.qty!==0){
-        return {
-          qty:prevState.qty-1
-        }
-      }
-      
-    });
-  }
-  render () {
-    const { price, title, qty } = this.state;
-    return (
-      <div className="cart-item">
-        <div className="left-block">
-          <img style={styles.image} />
-        </div>
-        <div className="right-block">
-          <div style={ { fontSize: 25 } }>{title}</div>
-          <div style={ { color: '#777' } }>Rs {price} </div>
-          <div style={ { color: '#777' } }>Qty: {qty} </div>
-          <div className="cart-item-actions">
-            {/* Buttons */}
-            <img
-              alt="increase"
-              className="action-icons"
-              src="https://img.icons8.com/emoji/48/000000/plus-emoji.png"
-              onClick={this.increaseQuantity}
-            />
-            <img
-              alt="decrease"
-              className="action-icons"
-              src="https://img.icons8.com/emoji/48/000000/minus-emoji.png"
-              onClick={this.decreaseQuantity}
-            />
-            <img
-              alt="delete"
-              className="action-icons"
-              src="https://img.icons8.com/ios-glyphs/30/000000/filled-trash.png"
-            />
-          </div>
+const CartItem = (props) => {
+  const { price, title, qty } = props.product;
+  const {
+    product,
+    onIncreaseQuantity,
+    onDecreaseQuantity,
+    onDeleteProduct
+  } = props;
+  return (
+    <div className="cart-item">
+      <div className="left-block">
+        <img style={styles.image} src={product.img} />
+      </div>
+      <div className="right-block">
+        <div style={ { fontSize: 25 } }>{title}</div>
+        <div style={ { color: '#777' } }>Rs {price} </div>
+        <div style={ { color: '#777' } }>Qty: {qty} </div>
+        <div className="cart-item-actions">
+          {/* Buttons */}
+          <img
+            alt="increase"
+            className="action-icons"
+            src="https://img.icons8.com/emoji/48/000000/plus-emoji.png"
+            onClick={() => onIncreaseQuantity(product)}
+          />
+          <img
+            alt="decrease"
+            className="action-icons"
+            src="https://img.icons8.com/emoji/48/000000/minus-emoji.png"
+            onClick={() => onDecreaseQuantity(product)}
+          />
+          <img
+            alt="delete"
+            className="action-icons"
+            src="https://img.icons8.com/ios-glyphs/50/000000/filled-trash.png"
+            onClick={() => onDeleteProduct(product.id)}
+          />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 const styles = {
