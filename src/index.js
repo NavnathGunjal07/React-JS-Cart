380808819289c726e1ea9a64b304720ddf4ef9ab
NavnathGunjal07 <53387548+NavnathGunjal07@users.noreply.github.com>
2022-03-17 19:30:33 +0530

    added firebase db

--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD3Cp5WBn2v3N9qKm5SZHVW70BKaGggKAQ",
+    authDomain: "cart-3363d.firebaseapp.com",
+    projectId: "cart-3363d",
+    storageBucket: "cart-3363d.appspot.com",
+    messagingSenderId: "901452867406",
+    appId: "1:901452867406:web:eb29749bbfadfab7d0601f"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
